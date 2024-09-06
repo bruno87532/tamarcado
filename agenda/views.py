0dcf92d529ac1816cb7e58fc073663397207f718
Bruno Guinerio <brunoguinerio1204@gmail.com>
2024-09-06 17:16:25 -0300

    Adicionando o método POST aos agendamentos seguindo os padrões de API REST

--- agenda/views.py
+++ agenda/views.py
@@ -12,8 +12,22 @@ def agendamentosDetail(request, id):
     serializer = AgendamentoSerializer(obj)
     return JsonResponse(serializer.data)
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def agendamentoList(request):
-    qs = Agendamento.objects.all()
-    serializer = AgendamentoSerializer(qs, many=True)
-    return JsonResponse(serializer.data, safe=False )
+    if request.method == "GET":
+        qs = Agendamento.objects.all()
+        serializer = AgendamentoSerializer(qs, many=True)
+        return JsonResponse(serializer.data, safe=False )
+    if request.method == "POST":
+        data = request.data  
+        serializer = AgendamentoSerializer(data=data)
+        if serializer.is_valid():
+            validatedData = serializer.validated_data
+            Agendamento.objects.create(
+                dataHorario = validatedData["dataHorario"],
+                nomeCliente = validatedData["nomeCliente"],
+                emailCliente = validatedData["emailCliente"],
+                telefoneCliente = validatedData["telefoneCliente"],
+            )
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
